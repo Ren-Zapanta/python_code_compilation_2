ffd07be717d93ff68a7dac05aa7d7c582f1e18eb
Ren <renzapnta4@gmail.com>
2025-03-21 22:15:18 +0800

    Pseudocode

--- batch_5/input_username.py
+++ batch_5/input_username.py
@@ -1,4 +1,4 @@
 #Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
-input_username = input("Please enter your full name after several spaces: ")
-trimmed_input = input_username.lstrip()
-print(trimmed_input)    
+input_username = input("Please enter your full name after several spaces: ") #Requests input from the user
+trimmed_input = input_username.lstrip() #Removes the leading spaces
+print(trimmed_input) #Prints the result
