4b4b2bc78a778e2ed69fa20fb43799addfa280c5
Ren <renzapnta4@gmail.com>
2025-03-22 01:30:39 +0800

    Statement fixes

--- batch_5/input_character_count.py
+++ batch_5/input_character_count.py
@@ -1,4 +1,4 @@
 #Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 user_input = input("Please input a statement: ") #Requests a statement from the user as the input.
 char_count = len(user_input) #Determines character count within the input.
-print(f"There are {char_count} words in your statement.") #Prints the number of characters
+print(f"There are {char_count} characters in your statement.") #Prints the number of characters
