c4ded0930be777c536da1d0d3ed5d16cd4754eec
Ren <renzapnta4@gmail.com>
2025-03-21 22:01:28 +0800

    removes the leading spaces

--- batch_5/input_username.py
+++ batch_5/input_username.py
@@ -1,3 +1,4 @@
 #Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 input_username = input("Please enter your full name after several spaces: ")
-print(input_username)
+trimmed_input = input_username.lstrip()
+print(trimmed_input)
