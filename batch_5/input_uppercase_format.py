0106be4655b1b6f042af0dba17992bd48afa813e
Ren <renzapnta4@gmail.com>
2025-03-22 00:10:19 +0800

    Reformats the input to all uppercase letters and prints it

---
+++ batch_5/input_uppercase_format.py
@@ -0,0 +1,4 @@
+#Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+user_input = input("Please input your full name: ") #Requests input from the user
+reformat_input = user_input.upper() #Reformats the input to all upercase letters
+print(reformat_input) #Prints the reformated text
