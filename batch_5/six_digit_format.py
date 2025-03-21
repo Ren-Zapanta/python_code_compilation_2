5df36a3d9976d4d0113b1f8832b10d60e0d8447d
Ren <renzapnta4@gmail.com>
2025-03-21 22:14:32 +0800

    Formats the user input to 6 digit and prints it

--- batch_5/six_digit_format.py
+++ batch_5/six_digit_format.py
@@ -1,2 +1,4 @@
 #Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
-user_input = int(input("Please input a number (0-1000): "))
+user_input = int(input("Please input a number (0-1000): ")) #Asks the user for input
+formatted_number = f'{user_input:06d}' #Formats input to 6 digits
+print(formatted_number) #Prints result
