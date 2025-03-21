1fc1998897fd67de9ff6164977cdd8699631dacf
Ren <renzapnta4@gmail.com>
2025-03-22 00:42:53 +0800

    Requests input from the user. Reformats the input to all lowercase letters and prints it

--- batch_5/input_lowercase_format.py
+++ batch_5/input_lowercase_format.py
@@ -1,4 +1,4 @@
 #Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
-user_input = input("Please enter your name in proper casing: ")
-format_input = user_input.lower()
-print(format_input)
+user_input = input("Please enter your name in proper casing: ") #Requests input from the user
+format_input = user_input.lower() #Reformats the user input to lowercasing
+print(format_input) #Prints the result
